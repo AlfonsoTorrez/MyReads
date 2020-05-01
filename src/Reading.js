8a192c227bfba530e30f092492be93f83a3971d4
AlfonsoTorrez <alftorres@csumb.edu>
2020-05-01 16:18:51 -0700

    refactor: added shelfType attribute to books

--- src/Reading.js
+++ src/Reading.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ShelfOptions from './ShelfOptions.js'
 
-const shelfType = "Reading";
 
 class Reading extends React.Component{
   state = {
@@ -9,12 +8,14 @@ class Reading extends React.Component{
       {
         image: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
         title: "To Kill a Mockingbird",
-        author: "Harper Lee"
+        author: "Harper Lee",
+        shelfType: "Reading"
       },
       {
         image: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
         title: "Ender's Game",
-        author: "Orson Scott Card"
+        author: "Orson Scott Card",
+        shelfType: "Reading"
       },
     ],
   }
@@ -31,7 +32,7 @@ class Reading extends React.Component{
                   <div className="book">
                     <div className="book-top">
                       <img className="book-cover" src={book.image} alt={book.title}/>
-                      <ShelfOptions shelfType={shelfType}/>
+                      <ShelfOptions shelfType={book.shelfType}/>
                     </div>
                     <div className="book-title">{book.title}</div>
                     <div className="book-authors">{book.author}</div>
