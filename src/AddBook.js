06c0c83cbb52be4ac14a2d1e0377f17057044c86
AlfonsoTorrez <alftorres@csumb.edu>
2020-05-01 17:55:53 -0700

    test: testing out search bar functiionality

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import serializeForm from 'form-serialize'
+// import * as BooksAPI from './BooksAPI'
 
 class AddBook extends React.Component{
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const value = serializeForm(e.target, { hash: true })
+    console.log("You search for:")
+    console.log(value.search)
+  }
+
   render(){
     return(
       <div className="search-books">
@@ -9,17 +19,19 @@ class AddBook extends React.Component{
           <Link className="close-search" to='/'>
             Close
           </Link>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+          <form onSubmit={this.handleSubmit}>
+            <div className="search-books-input-wrapper">
+              {/*
+                NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                You can find these search terms here:
+                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-          </div>
+                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                you don't find a specific author or title. Every search is limited by search terms.
+              */}
+              <input type="text" name='search' placeholder="Search by Genre"/>
+            </div>
+          </form>
         </div>
         <div className="search-books-results">
           <ol className="books-grid"></ol>
